504e26f29123ee14e5ff05a694ed10001497f5bd
seginusmowlavi <seginus.mowlavi@gmail.com>
2023-01-12 18:53:52 +0100

    premiers pas
    classe automate *presque* finie
    le reste est à faire, cf readme

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ Projet MVA S1 pour J. Delon, Y. Gousseau
 
 [Descriptif du projet](https://perso.telecom-paristech.fr/gousseau/MVA/Projets2022/TextureAC/)
 
-blaabhalbha
+A faire :
+1. stochastic update !
+2. load un pretrained VGG, il faut juste les outputs des hidden layers en forward
+3. comme ça on peut définir la loss pour le training (texture loss)
+4. ...
+5. quand tout est fini: on peut implémenter training avec inception loss
---
+++ model.py
@@ -0,0 +1,72 @@
+import numpy as np
+import torch
+from torch import nn
+
+
+class CAutomaton(nn.Module):
+
+    def __init__(self, num_hidden_states=9, num_hidden_features=96):
+        '''
+        parameters:
+            num_hidden_states = positive int, number of hidden cell-states
+            num_hidden_features = positive int, number of channels in hidden layer
+        '''
+        super().__init__()
+
+        # size of the model
+        self.num_states = 3+num_hidden_states
+        self.num_hidden_features = num_hidden_features
+
+        # layers
+        self.perception_filter = nn.Conv2d(self.num_states,4*self.num_states,
+                                                 kernel_size=3,
+                                                 padding_mode='circular',
+                                                 bias=False)
+        self.update_rule = nn.Sequential(nn.Conv2d(4*self.num_states,
+                                                   self.num_hidden_features,
+                                                   kernel_size=1,
+                                                   padding_mode='circular'),
+                                         nn.ReLU(),
+                                         nn.Conv2d(self.num_hidden_features,
+                                                   self.num_states,
+                                                   kernel_size=1,
+                                                   padding_mode='circular'))
+        '''
+        IMPLEMENT STOCHASTIC UPDATE
+        '''
+
+    def forward(self, x):
+        perception = self.perception_filter(x)
+        updated = self.update_rule(perception)
+        return updated
+
+def set_perception_kernels(automaton):
+    '''
+    Sets the perception filter to concat(identity,sobels,laplacian)
+    Since this filter is not learned: requires_grad=False
+    '''
+    n = automaton.num_states
+    kernel = np.zeros((4*n,n,3,3)) # shape: (out_ch,in_ch,H,W)
+    kernel[np.arange(n),np.arange(n),1,1] = 1 # first channels: cell-state
+    kernel[np.arange(n,2*n),np.arange(n),:,:] = np.array([[-1,0,1],
+                                                          [-2,0,2],
+                                                          [-1,0,1]]) # Kx
+    kernel[np.arange(2*n,3*n),np.arange(n),:,:] = np.array([[-1,-2,-1],
+                                                            [ 0, 0, 0],
+                                                            [ 1, 2, 1]]) # Ky
+    kernel[np.arange(3*n,4*n),np.arange(n),:,:] = np.array([[1,  2,1],
+                                                            [2,-12,2],
+                                                            [1,  2,1]]) # Klap
+    automaton.perception_filter.weight = nn.parameter.Parameter(kernel,requires_grad=False)
+
+def train(automaton, template, rate=[2e-3]*2000+[2e-4]*6000,
+                               step_min=32,
+                               step_max=64):
+    '''
+    Train an automaton with the following parameters:
+        template = image
+        rate = list of learning rates (note that #epochs = len(rate))
+        (step_min,step_max) : range of the number of automaton evolution steps
+                              for each training sample
+    '''
+    raise NotImplementedError
